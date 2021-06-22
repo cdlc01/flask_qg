f6bd5a83f218db2b8f7ef5dde09f4704f2f69062
Chris de la Cruz <77891283+cdlc01@users.noreply.github.com>
2021-06-21 21:11:59 -0400

    Update predictor.py
    Updated get prediction function

--- functional_app/src/models/predictor.py
+++ functional_app/src/models/predictor.py
@@ -8,7 +8,12 @@ def get_prediction(feature_values):
     # Model is expecting a list of lists, and returns a list of predictions
     predictions = loaded_model.predict(feature_values)
     # We are only making a single prediction, so return the 0-th value
-    return predictions[0]
+    #return predictions[0]
+    
+    if predictions[0] == 0:
+        return print("The model predicts that this is not hate speech")
+    else:
+        return print("The model predicts that this is hate speech")
 
 def un_pickle_model():
     """ Load the model from the .pkl file """
