b26836b2c5675b71a86450b266dc31b6c9611b47
Chris de la Cruz <77891283+cdlc01@users.noreply.github.com>
2021-06-21 21:31:23 -0400

    Update utils.py
    fixed function

--- functional_app/src/utils.py
+++ functional_app/src/utils.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import requests
 
 def extract_feature_values(data):
     """ Given a params dict, return the values for feeding into a model"""
     
-    tweet = requests.form['tweet']
+    tweet = data['tweet']
     
     # Replace these features with the features for your model. They need to 
     # correspond with the `name` attributes of the <input> tags
