0f071b56a25fc0811fce5b943c6dc806b6080cf5
Chris de la Cruz <77891283+cdlc01@users.noreply.github.com>
2021-06-22 08:03:04 -0400

    Update app.py
    Experiment #2

--- functional_app/app.py
+++ functional_app/app.py
@@ -35,6 +35,7 @@ def show_results():
     
     # Extract the prediction from the URL params
     #prediction = request.args.get("prediction")
+    prediction = get_prediction(feature_values)
 
     # Return the results pge
     return render_template("results.html", prediction=prediction)
